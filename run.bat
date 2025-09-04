f75c512ace3dc41e0805d4b2e6dcfeea6d4e64d5
Kevin Arias <kearias@alumno.unlam.edu.ar>
2025-09-04 18:24:30 -0300

    agregar tabla a script bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 flex Lexico.l
 bison -dyv Sintactico.y
 
-gcc.exe lex.yy.c y.tab.c -o lyc-compiler-1.0.0.exe
+gcc.exe lex.yy.c y.tab.c tabla.c -o lyc-compiler-1.0.0.exe
 
 lyc-compiler-1.0.0.exe prueba.txt
 
